46993815b80b44fb8cc8a95a11e1cfdfda4b766d
Manouvellemission <hello@getintalent.com>
2025-07-11 13:53:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1485,6 +1485,10 @@ const JobBoardContent = () => {
             </div>
           </div>
         )}
+    </div>
+ </div>
+);
+};         
 
 // ==================== WRAPPER AVEC PROVIDERS ====================
 const App = () => {
